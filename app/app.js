ea5a5f353efa5c423422b80860e5b48d073bc1bf
Luke Schuyler <lukeschuyler1@gmail.com>
2017-01-23 14:40:12 -0600

    ready to split up project

---
+++ app/app.js
@@ -0,0 +1,30 @@
+const app = angular.module('manApp', ['ngRoute', 'ngMaterial']);
+
+app.config(function($routeProvider, $locationProvider) {
+	$locationProvider.hashPrefix('')
+
+	$routeProvider
+	   .when('/', {
+	   	templateUrl : '/app/partials/main.html',
+	   	controller : 'MainCtrl'
+	   })
+	   .when('/search/pins/:query', {
+	   	templateUrl : '/app/partials/search.html',
+	   	controller : 'MainCtrl'
+	   })
+	   .when('/pin/:pinId', {
+	   	templateUrl : '/app/partials/pin.html',
+	   	controller : 'PinCtrl'
+	   })
+	   .when('/user/boards', {
+	   	templateUrl : '/app/partials/userBoards.html',
+	   	controller : 'UserBoardsCtrl'
+	   })
+	   .when('user/:boardName', {
+	   	templateUrl : '/app/partials/posted.html',
+	   	controller : 'PostedCtrl'
+	   })
+	   .otherwise({
+	   	redirectTo : '/'
+	   })
+})
