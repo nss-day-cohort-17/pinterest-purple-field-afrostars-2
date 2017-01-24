e07121230c8b1d86c9ae8bd65b5d7570e5a1aa81
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-24 14:51:11 -0600

    adding factory to share index info

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -1,10 +1,12 @@
-app.controller('MainCtrl', function($scope, $http) {
+app.controller('MainCtrl', function($scope, $http, modalFactory) {
 	$http.defaults.headers.common["Api-Key"] = '8q74e5hd4drev8c9wwbm2zhf';
 	$http.get('https://api.gettyimages.com/v3/search/images?fields=id,title,referral_destinations,comp&sort_order=best&phrase=football')
 	.then(function(val){
 		console.log(val)
 		$scope.images = val.data.images
 	})
+
+	$scope.logIndex = function(indx) {console.log('indx', indx)}
 
 	$(document).ready(function() {
 		$('.modal').modal()
