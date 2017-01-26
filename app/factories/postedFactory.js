0424da954b168c7a26431ab818edd44eb1684659
TSturtledove <roberttortorelli@yahoo.com>
2017-01-26 09:48:18 -0600

    got the pins into an array, working on sending that information out to use for filtering the pinsdata

--- app/factories/postedFactory.js
+++ app/factories/postedFactory.js
@@ -1,3 +1,6 @@
+//*********************************************
+//      Reference of previous try at code
+//*******************************************
 // app.factory('postedFactory', function($http) {
 //   return {
 //     getPersonalBoard : () => {
@@ -9,12 +12,12 @@
 //     }
 //   }
 // })
-
+//*******************************************************
+//********************************************************
 
 
 app.factory('postedFactory', function($routeParams, userBoardsFactory, $http) {
   return {
-    // var pinbook = []
         getPersonalBoard : () => {
           userBoardsFactory.getBoards()
           .then((val) =>{
@@ -25,22 +28,40 @@ app.factory('postedFactory', function($routeParams, userBoardsFactory, $http) {
             name = $routeParams.boardName //saves the name of the board
             console.log($routeParams.boardName)
             console.log(name)
+            // console.log(boarddata)
 
-            console.log(boarddata)
-
-
-            // var arraything = [];
             var result = {};
             // var re = [];
+            var boardTitle
+            var boardPins
+
             angular.forEach(boarddata, function(value, key) {
 
                 result[key] = value;
+                // console.log("this is a key" + key)
+                // console.log("this is a value" + value)
+                // console.log("this is result[key]" + result[key])
                 boardPins = result[key].pins
                 boardTitle = result[key].title
                 console.log(boardTitle)
                 if( boardTitle == name) {
                   console.log (boardTitle + " = " + name)
                   console.log(boardPins)
+
+                  var ultimateGoal = {};
+                  var picture
+                  var arraything = [];
+
+                  angular.forEach(boardPins, function(pinNumber, pinId) {
+                    picture = pinNumber.pin_id
+                    // console.log(pinNumber)
+                    // console.log("this is the pin" + pinNumber)
+                    console.log("this is the pinId " + picture)
+                    console.log(picture)
+                    this.push(picture);
+                  }, arraything);
+                  console.log(arraything)
+
 
                 }
 
@@ -53,15 +74,29 @@ app.factory('postedFactory', function($routeParams, userBoardsFactory, $http) {
   }
 })
 
-
+//***************************************
+//      Notes
+//***************************************
 // needs to get the board by title, so with an if function probably.
 // something like if(boards[i].title === $scope.pinBoard)
 // $scope.pictures= boards[i].pins
 // might then loop through all the pins and make an array of pins with
 // matching id's, then post that array
+//*******************************************************
+//****************************************************
 
 
-
+//**************************************
+//      Reference code for .push function
+//*****************************************
+// var values = {name: 'misko', gender: 'male'};
+// var log = [];
+// angular.forEach(values, function(value, key) {
+//   this.push(key + ': ' + value);
+// }, log);
+// expect(log).toEqual(['name: misko', 'gender: male']);
+//**********************************************************
+//********************************************************
 
 
 
