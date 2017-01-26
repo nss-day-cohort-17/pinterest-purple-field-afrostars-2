a2f3e48afc4740ca68c836ec352387c45fe9154a
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-26 13:12:57 -0600

    comment out duplicate factory

--- app/factories/pinFactory.js
+++ app/factories/pinFactory.js
@@ -5,18 +5,18 @@
 
 
 
-app.factory('userBoardsFactory', function($http) {
-  return {
-    getBoards : () => {
-      return $http.get('https://pinterestclone-24ce7.firebaseio.com/boards.json')
-      .then((value) => {
-        // console.log(value)
-        return value.data
-      })
-    },
-    addPin: (boardId, pinId) => {
-			let data = {pin_id: pinId}
-			$http.post(`https://pinterestclone-24ce7.firebaseio.com/boards/${boardId}/pins.json`, JSON.stringify(data))
-		}
-  }
-})
+// app.factory('userBoardsFactory', function($http) {
+//   return {
+//     getBoards : () => {
+//       return $http.get('https://pinterestclone-24ce7.firebaseio.com/boards.json')
+//       .then((value) => {
+//         // console.log(value)
+//         return value.data
+//       })
+//     },
+//     addPin: (boardId, pinId) => {
+// 			let data = {pin_id: pinId}
+// 			$http.post(`https://pinterestclone-24ce7.firebaseio.com/boards/${boardId}/pins.json`, JSON.stringify(data))
+// 		}
+//   }
+// })
